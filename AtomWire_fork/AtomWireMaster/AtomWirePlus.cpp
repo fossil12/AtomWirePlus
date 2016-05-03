de6eea2df1e1be8995e4f8a32bc881a08256bb2c
David Keller <dev@davidkeller.me>
2016-05-03 16:02:35 -0400

    Single frame send and receive methods

--- AtomWire_fork/AtomWireMaster/AtomWirePlus.cpp
+++ AtomWire_fork/AtomWireMaster/AtomWirePlus.cpp
@@ -1,65 +1,170 @@
 #include "AtomWirePlus.h"
 
-AtomWirePlus::AtomWirePlus(uint8_t pin) : OneWire(pin) {}
+AtomWirePlus::AtomWirePlus(uint8_t pin) : OneWire(pin) {
+  // Does that work like that?
+  num_nodes = 0;
+  round = 0;
+}
+
+// Assumes msg is 64 bits (8 bytes) long
+uint8_t AtomWirePlus::send_msg_p(uint8_t msg[64])
+{
+  uint8_t frame[FRAME_BYTE_LENGTH];
+
+  // Send bits are 0x7
+  uint8_t cmd_frag = 0x70;
+
+  // Check if message is empty
+  if (msg == NULL) {
+    this->write(cmd_frag); // send command indicating empty message
+    return TRUE;
+  }
+
+  // We currently only support unfragmented messages
+  cmd_frag &= 0x8;
+
+  frame[0] = cmd_frag;
+
+  // Size/offset
+  frame[1] = 0x01; // 8 bytes;
+  frame[2] = 0x00; // end of size/offset
+
+  // frag_id: for now the round variable
+  frame[2] |= (round & 0x10) >> 4;
+  frame[3] = 0x00;
+  frame[3] |= (round & 0x01) << 4;
+
+  // payload of message
+  for (int i = 0; i < 8; i++) {
+    frame[i + 4] = msg[i];
+  }
+
+  frame[12] = this->crc8(msg, 8);
+  return TRUE;
+}
+
+uint8_t AtomWirePlus::recv_msg_p(uint8_t msg[64])
+{
+  uint8_t frame[FRAME_BYTE_LENGTH];
+
+  // recev bits 0x90
+  this->write(0x90); // Receive request sent to sender
+  this->read_bytes(frame, 1);
+
+  // Check if there is anything to receive
+  if (frame[0] == 0x90) {
+    return TRUE;
+  }
+
+  // Get rest of frame
+  this->read_bytes(&frame[1], FRAME_BYTE_LENGTH - 1);
+
+  // Check if it's a send request
+  if ((frame[0] & 0x10) != 0x90) {
+    return FALSE;
+  }
+
+  // TODO: Check for fragmentation and start/continue reassembly if true
+
+  // Copy received message into message buffer
+  for (int i = 0; i < 8; i++) {
+    msg[i] = frame[i + 4];
+  }
+
+  return TRUE;
+}
 
 uint8_t AtomWirePlus::run(void)
 {
-  if (sender_addr != NULL && sender_msg != NULL) {
-    recvr_addr = sender_addr;
-    recvr_msg = sender_msg;
-
-    sender_addr = NULL;
-    sender_msg = NULL;
-
-    return TRUE;
-  } else {
-    return FALSE;
-  }
-}
-
-uint8_t AtomWirePlus::get_node(uint8_t pos, uint8_t *addr)
-{
-  bool serachExpired = 0;
-
-  if (serachExpired) {
+  if ((round % REDUE_SEARCH_INTERVAL) == 0) {
     full_search();
   }
 
-  return this->search(addr);
+  round++;
+
+  // Nothing to do if there are no nodes on the line
+  if (num_nodes == 0) {
+    return TRUE;
+  } else if (current_node >= num_nodes) {   // If nodes were removed start at node 0 again
+    current_node = 0;
+  } else {
+    current_node = ++current_node % num_nodes;
+  }
+
+  // reset/presence pulse
+  if (!this->reset()) {
+    return FALSE;
+  }
+
+  // select address
+  select(addrs[current_node]);
+
+  // Time to send message if there is any
+  // TODO: Do fragmenting before call of send_msg_p and pass important parameters
+  if (!send_msg_p(out_msg_queue[current_node])) {
+    return FALSE;
+  }
+
+  // Time to receive message
+  // TODO: Check for fragmenting and start assembling incoming message
+  if (!recv_msg_p(in_msg_queue[current_node])) {
+    return FALSE;
+  }
+
+  return TRUE;
 }
+
+// uint8_t AtomWirePlus::get_node(uint8_t pos, uint8_t *addr)
+// {
+//   // bool serachExpired = 0;
+
+//   // if (serachExpired) {
+//   //   full_search();
+//   // }
+
+//   return this->search(addr);
+// }
 
 uint8_t AtomWirePlus::send_msg(uint8_t *addr, uint8_t *msg)
 {
-  if (addr != NULL) {
-    sender_addr = addr;
-    sender_msg = msg;
+  // Broadcast if *addr = 0x0000 0000 0000 0000
 
-    return TRUE;
-  } else {
-    return FALSE;
-  }
+  // queue message if not
+
+  // if (addr != NULL) {
+  //   sender_addr = addr;
+  //   sender_msg = msg;
+
+  //   return TRUE;
+  // } else {
+  //   return FALSE;
+  // }
+
+  return FALSE;
 }
 
 uint8_t AtomWirePlus::recv_msg(uint8_t *addr, uint8_t *msg)
 {
-  if (recvr_addr != NULL && recvr_msg != NULL) {
-    addr = recvr_addr;
-    msg = recvr_msg;
+  // if (recvr_addr != NULL && recvr_msg != NULL) {
+  //   addr = recvr_addr;
+  //   msg = recvr_msg;
 
-    recvr_addr = NULL;
-    recvr_msg = NULL;
+  //   recvr_addr = NULL;
+  //   recvr_msg = NULL;
 
-    return TRUE;
-  } else {
-    return FALSE;
-  }
+  //   return TRUE;
+  // } else {
+  //   return FALSE;
+  // }
+
+  return FALSE;
 }
 
 void AtomWirePlus::full_search(void)
 {
-  uint8_t *addr;
+  // uint8_t *addr;
 
-  for (int i = 100; i < 0; i--) {
-    this->search(addr);
-  }
+  // for (int i = 100; i < 0; i--) {
+  //   this->search(addr);
+  // }
 }
--- AtomWire_fork/AtomWireMaster/AtomWirePlus.h
+++ AtomWire_fork/AtomWireMaster/AtomWirePlus.h
@@ -3,30 +3,57 @@
 
 #include "AtomWire.h"
 
+#define REDUE_SEARCH_INTERVAL 4
+#define MAX_BLOCKS_ON_LINE 10
+#define FRAME_BYTE_LENGTH 13
+
 class AtomWirePlus : public OneWire
 {
   private:
+    // number of connected nodes
+    uint8_t num_nodes;
+
+    // Node selected for this round (index in `addrs`)
+    uint8_t current_node;
+
+    // Incremented at after every run of a time slot
+    uint8_t round;
+
     // store all nodes (followers) on the line
-    uint8_t *addrs[];
+    uint8_t addrs[MAX_BLOCKS_ON_LINE][8];
+
+    // Store up to one message per node to be sent out
+    // Same index as in `addrs` 
+    uint8_t *out_msg_queue[]; // nothing if entry is NULL
+
+    // Queue up to one message per node that was received
+    // Same index as in `addrs`
+    uint8_t *in_msg_queue[]; // nothing if entry is NULL
 
     // sending buffer
-    uint8_t *sender_addr;
-    uint8_t *sender_msg;
+    //uint8_t *sender_addr;
+    //uint8_t *sender_msg;
 
     // receving buffer
-    uint8_t *recvr_addr;
-    uint8_t *recvr_msg;
+    //uint8_t *recvr_addr;
+    //uint8_t *recvr_msg;
+
+    uint8_t send_msg_p(uint8_t msg[64]);
+    uint8_t recv_msg_p(uint8_t msg[64]);
+    uint8_t get_pos_of_node(uint8_t addr[64]);
 
   public:
     AtomWirePlus(uint8_t pin);
 
     // runs a time slot and returns
     uint8_t run(void); // needs scheduling
-    uint8_t get_node(uint8_t pos, uint8_t *addr);
+    //uint8_t get_node(uint8_t pos, uint8_t *addr);
 
+    // No guarantee, that the message will be immediatly sent, it might get queued
     uint8_t send_msg(uint8_t *addr, uint8_t *msg);
     uint8_t recv_msg(uint8_t *addr, uint8_t *msg);
 
+    // We assume that only node at the end of the array are removed or added
     void full_search(void);
 };
 

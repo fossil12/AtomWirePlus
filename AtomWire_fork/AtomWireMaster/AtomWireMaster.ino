37c516192c45ff184421d7277bfae1914c50ee6c
David Keller <dev@davidkeller.me>
2016-05-05 15:06:32 -0400

    Use different buffers for out and in messages in master demo

--- AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
+++ AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
@@ -3,16 +3,19 @@
 AtomWirePlus awm(12);
 int c = 0;
 byte data[8];
+byte data_out[8];
 
 void setup(void) {
   Serial.begin(9600);
-  data[7] = 0x00;
+  data_out[7] = 0x00;
   Serial.print("\n======== Begin ========\n");
 }
 
 void loop(void) {
   byte i;
   byte addr[8];
+  
+  data_out[7]++;
 
   Serial.print("\n-----------------------\n\n");
 
@@ -26,17 +29,17 @@ void loop(void) {
     Serial.print(addr[i], HEX);
     Serial.print(" ");
   }
-  Serial.print("\n");
+  Serial.print("\n\n");
 
-  awm.send_msg(addr, data);
+  awm.send_msg(addr, data_out);
   awm.run();
   
   if (awm.recv_msg(addr, data) == -2) {
-    Serial.print("No message received...\n");
+    Serial.print("\nNo message received...\n");
     return;
   }
 
-  Serial.print("Msg = ");
+  Serial.print("\nMsg = ");
   for (i = 0; i < 8; i++) {
     Serial.print(data[i], HEX);
     Serial.print(" ");

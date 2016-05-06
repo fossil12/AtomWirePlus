da370a321f10d3711417cb59dba39c4dcdedc24d
David Keller <dev@davidkeller.me>
2016-05-06 04:48:33 -0400

    Working implementation 🎉

--- AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
+++ AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
@@ -10,7 +10,7 @@ byte broadcast_addr[8];
 #define NUM_LINES 4
 #define MAX_INPUT_LENGTH 8
 
-//AtomWirePlus **awms;
+AtomWirePlus **awms;
 byte input_recved = 0;
 int input[MAX_INPUT_LENGTH];
 
@@ -28,11 +28,11 @@ void setup(void) {
   }
 
   // Games...
-//  awms = new AtomWirePlus*[NUM_LINES];
-//  byte offset = 10;
-//  for (i = 0; i < NUM_LINES; i++) {
-//    awms[i] = new AtomWirePlus(i + offset);
-//  }
+  awms = new AtomWirePlus*[NUM_LINES];
+  byte offset = 10;
+  for (i = 0; i < NUM_LINES; i++) {
+    awms[i] = new AtomWirePlus(i + offset);
+  }
   
   //Serial.print("\n======== Begin ========\n");
 }
@@ -117,7 +117,7 @@ void loop(void) {
   /* ================================================================================
    * Messqe queue
    * ================================================================================ */
-  int j;
+  /*int j;
 
   awm.run_all();
 
@@ -158,12 +158,12 @@ void loop(void) {
 
   Serial.print("\nNo more messages...\n\n");
 
-  delay(1500); // 1500 miliseconds (1.5 sec)
+  delay(1500); // 1500 miliseconds (1.5 sec) */
 
   /* ================================================================================
    * Games
    * ================================================================================ */
-  /*int j;
+  int j;
   
   for (i = 0; i < NUM_LINES; i++) {
     byte num_nodes = awms[i]->run_all();
@@ -195,7 +195,7 @@ void loop(void) {
       input_recved--;
     }
     
-  } */
+  }
   
 }
 

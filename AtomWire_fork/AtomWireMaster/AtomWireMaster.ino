fc30aad27bc9f9ae33c4c1086b2004ea89af58e2
David Keller <dev@davidkeller.me>
2016-05-05 22:20:46 -0400

    Master main program for checking of gpio messages

--- AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
+++ AtomWire_fork/AtomWireMaster/AtomWireMaster.ino
@@ -16,8 +16,12 @@ void loop(void) {
   byte i;
   byte addr[8];
 
-  data_out[6]++;
-  data_out[7]++;
+  /* ================================================================================
+   * Send messge around
+   * ================================================================================ */
+  /*
+  data_out[6] += 1;
+  data_out[7] += 1;
 
   Serial.print("\n-----------------------\n\n");
 
@@ -49,6 +53,42 @@ void loop(void) {
   Serial.print("\n");
 
   delay(1500); // 1500 miliseconds (1.5 sec)
+  */
+
+  /* ================================================================================
+   * Check of any incoming message
+   * ================================================================================ */
+
+  Serial.print("\n-----------------------\n\n");
+
+  if (!awm.get_next_node_addr(addr)) {
+    Serial.print("\nNo more nodes detected...\n");
+    return;
+  }
+
+  Serial.print("\nAddress = ");
+  for (i = 0; i < 8; i++) {
+    Serial.print(addr[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\n\n");
+
+  awm.run();
+  
+  if (awm.recv_msg(addr, data) == -2) {
+    Serial.print("\nNo message received...\n");
+    return;
+  }
+
+  Serial.print("\nMsg = ");
+  for (i = 0; i < 8; i++) {
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+
+  delay(1500); // 1500 miliseconds (1.5 sec)
+  
 }
 
 

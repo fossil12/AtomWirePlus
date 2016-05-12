eb8cfd0d1ab3d5dfcc3207b4452982e4d0e8e59b
risia <risi@seas.upenn.edu>
2016-05-11 20:25:51 -0400

    Add files via upload

---
+++ BlockGame/Scripts/Controls.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	
+	//dimensions of play field
+	public const int X_LENGTH = 5;
+	public const int Z_LENGTH = 5;
+	
+	public const int NUM_LINES = 3;
+	
+	public GameObject player;
+
+	public GameObject dummy;
+
+	public Text vicText;
+
+	public bool end;
+
+	public Vector3 start = new Vector3 (2, 1, 0);
+	//public Vector3 goal = new Vector3 (2, 5, 4);
+	public Vector3 goal = new Vector3 (2, 2, 2);
+
+	int[,] blocks = new int[X_LENGTH, Z_LENGTH];
+
+	Test blockScript;
+
+	// Use this for initialization
+	void Start () {
+		blockScript = dummy.GetComponent<Test>();
+		vicText.text = "";
+		end = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = player.transform.position;
+
+		if ((pos == goal)) {
+			if (!end)
+				victory ();
+			else {
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					end = false;
+					player.transform.position = start;
+					vicText.text = "";
+				}
+			}
+		} else {
+			move (pos);
+		}
+
+
+	}
+
+	void victory() {
+		vicText.text = "VICTORY!\nPress Space to Restart";
+		end = true;
+		byte[] output = { 0x21, 0x22};
+		blockScript.awInput.Write (output, 0, 2);
+		Debug.Log (output);
+	}
+
+	void move(Vector3 pos) {
+		blocks = blockScript.block_cnt;
+		pos.y = blocks [(int)pos.x, (int)pos.z] + 1;
+	
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			if (pos.x < (X_LENGTH - 1)) {
+				if (Physics.Raycast (pos, Vector3.right, 1)) {
+					if (blocks [(int)pos.x + 1, (int)pos.z] == (int)pos.y) {
+						pos.x++;
+						pos.y++;
+					}
+				} else {
+					pos.x++;
+					pos.y = blocks [(int)pos.x, (int)pos.z] + 1;
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if (pos.x > 0) {
+				if (Physics.Raycast (pos, Vector3.left, 1)) {
+					if (blocks [(int)pos.x - 1, (int)pos.z] == (int)pos.y) {
+						pos.x--;
+						pos.y++;
+					}
+
+				} else {
+					pos.x--;
+					pos.y = blocks [(int)pos.x, (int)pos.z] + 1;
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			if (pos.z < (Z_LENGTH - 1)) {
+				if (Physics.Raycast (pos, Vector3.forward, 1)) {
+					if (blocks [(int)pos.x, (int)pos.z + 1] == (int)pos.y) {
+						pos.z++;
+						pos.y++;
+					}
+				} else {
+					pos.z++;
+					pos.y = blocks [(int)pos.x, (int)pos.z] + 1;
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (pos.z > 0) {
+				if (Physics.Raycast (pos, Vector3.back, 1)) {
+					if (blocks [(int)pos.x, (int)pos.z - 1] == (int)pos.y) {
+						pos.z--;
+						pos.y++;
+					}
+
+				} else {
+					pos.z--;
+					pos.y = blocks [(int)pos.x, (int)pos.z] + 1;
+				}
+			}
+		}
+
+		player.transform.position = pos;
+	}
+}
---
+++ BlockGame/Scripts/Test.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class Test : MonoBehaviour {
+	
+	//dimensions of play field
+	public const int X_LENGTH = 5;
+	public const int Z_LENGTH = 5;
+
+	public const int NUM_LINES = 4;
+
+	public SerialPort awInput = new SerialPort("COM5", 9600);
+	public int[,] block_cnt = new int[X_LENGTH, Z_LENGTH];
+
+	public GameObject block;
+
+	public class Node
+	{
+		public Node prev;
+		public GameObject obj;
+
+	}
+
+	Node[,] blocks = new Node[X_LENGTH, Z_LENGTH];
+
+
+	// Use this for initialization
+	void Start () {
+		awInput.Open ();
+		for (int i = 0; i < X_LENGTH; i++) {
+			for (int j = 0; j < Z_LENGTH; j++) {
+				block_cnt [i, j] = 0;
+			}
+		}
+		//goal
+		block_cnt[2, 4] = 4;
+
+
+		//awInput.Write ("Hello World");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int status_raw;
+		int status;
+		int line;
+
+
+		status_raw = awInput.ReadByte ();
+
+		for (int i = 0; i < NUM_LINES; i++) {
+			while (status_raw != 0xFF) {
+				status_raw = awInput.ReadByte ();
+			}
+
+			line = awInput.ReadByte ();
+
+			status_raw = awInput.ReadByte ();
+			status = status_raw; //# of Nodes on line
+
+			status_raw = awInput.ReadByte (); //Node ID
+
+			if (status <= 5) {
+				for (;block_cnt [2, line] < status; block_cnt [2, line]++) {
+					Node temp = blocks [2, line];
+					blocks [2, line] = new Node ();
+					blocks [2, line].obj = (GameObject)Instantiate (block, new Vector3 (2, block_cnt [2, line] + 1, line), Quaternion.identity);
+					blocks [2, line].prev = temp;
+				}
+				for (; block_cnt [2, line] > status; block_cnt [2, line]--) {
+					Node temp = blocks [2, line];
+					blocks [2, line] = temp.prev;
+					Destroy (temp.obj);
+				}
+			}
+		}
+		awInput.BaseStream.Flush();
+	}
+}
